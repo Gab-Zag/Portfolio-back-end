8683024b3fd31c5e57e7203e7d5231fdd5c6fad7
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2026-03-24 22:45:33 -0300

    Restrict CORS to specific origin

--- src/main/java/com/gab/portfolio/config/cors/CorsConfig.java
+++ src/main/java/com/gab/portfolio/config/cors/CorsConfig.java
@@ -1,12 +1,14 @@
 package com.gab.portfolio.config.cors;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
+public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE","OPTIONS","PATCH").allowedHeaders("*");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://portfolio-five-rho-64.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*");
     }
 }
